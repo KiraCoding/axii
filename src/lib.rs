aa7a50b16f81faf7d6dfbd26e64d208c31b62298
KiraCoding <38864051+KiraCoding@users.noreply.github.com>
2024-07-19 14:34:46 +0200

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -3,33 +3,24 @@ compile_error!("This crate can only be compiled for the x86_64-pc-windows-msvc t
 
 use core::ffi::c_void;
 use core::mem::transmute;
-use std::thread::sleep;
-use std::time::Duration;
-use windows::core::{s, IUnknown, GUID};
-use windows::Win32::Foundation::{FreeLibrary, HINSTANCE, HMODULE};
+use windows::core::{s, w, IUnknown, GUID, HRESULT};
+use windows::Win32::Foundation::{HINSTANCE, HMODULE};
 use windows::Win32::System::Console::{AllocConsole, GetConsoleMode, SetConsoleMode};
-use windows::Win32::System::Console::{GetStdHandle, SetConsoleTitleA, SetStdHandle};
+use windows::Win32::System::Console::{GetStdHandle, SetConsoleTitleW, SetStdHandle};
 use windows::Win32::System::Console::{CONSOLE_MODE, ENABLE_VIRTUAL_TERMINAL_PROCESSING};
 use windows::Win32::System::Console::{STD_ERROR_HANDLE, STD_OUTPUT_HANDLE};
 use windows::Win32::System::LibraryLoader::DisableThreadLibraryCalls;
-use windows::Win32::System::LibraryLoader::{GetProcAddress, LoadLibraryA};
+use windows::Win32::System::LibraryLoader::{GetProcAddress, LoadLibraryW};
 use windows::Win32::System::SystemServices::{DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH};
 use windows::Win32::System::Threading::{CreateThread, THREAD_CREATION_FLAGS};
 
-type DWORD = u32;
-type LPVOID = *mut c_void;
-type DirectInput8CreateFunc = extern "system" fn(HINSTANCE, DWORD, GUID, LPVOID, IUnknown);
-
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
-static mut MODULE_PROXY: Option<HMODULE> = None;
-static mut MODULE_SYSTEM: Option<HMODULE> = None;
 static mut PROXY_FUNCTION: Option<fn()> = None;
 
 #[no_mangle]
 unsafe extern "system" fn DllMain(module: HMODULE, reason: u32, _: *mut c_void) -> bool {
     DisableThreadLibraryCalls(module).unwrap_unchecked();
-    MODULE_PROXY = Some(module);
 
     match reason {
         DLL_PROCESS_ATTACH => {
@@ -43,9 +34,7 @@ unsafe extern "system" fn DllMain(module: HMODULE, reason: u32, _: *mut c_void) 
             )
             .unwrap();
         }
-        DLL_PROCESS_DETACH => {
-            FreeLibrary(MODULE_SYSTEM.unwrap()).unwrap();
-        }
+        DLL_PROCESS_DETACH => {}
         _ => (),
     }
 
@@ -54,33 +43,29 @@ unsafe extern "system" fn DllMain(module: HMODULE, reason: u32, _: *mut c_void) 
 
 unsafe extern "system" fn init(_: *mut c_void) -> u32 {
     init_console();
+    init_tracing();
     init_proxy();
 
-    sleep(Duration::from_secs(10));
+    // let module = LoadLibraryA(s!("aard.dll")).unwrap();
+    // let entry = GetProcAddress(module, s!("plugin")).unwrap();
 
-    let module = LoadLibraryA(s!("aard.dll")).unwrap();
-    let entry = GetProcAddress(module, s!("plugin")).unwrap();
-
-    let true_entry: unsafe fn() = transmute(entry);
-    true_entry();
+    // let true_entry: unsafe fn() = transmute(entry);
+    // true_entry();
 
     true as u32
 }
 
 unsafe fn init_proxy() {
-    MODULE_SYSTEM = Some(LoadLibraryA(s!("C:\\Windows\\System32\\dinput8.dll")).unwrap());
-    println!("[INIT] Loaded dinput8.dll at {}", MODULE_SYSTEM.unwrap().0);
+    let module = LoadLibraryW(w!("C:\\Windows\\System32\\dinput8.dll")).unwrap();
+    println!("[INIT] Loaded dinput8.dll at {}", module.0);
 
-    PROXY_FUNCTION = Some(transmute(GetProcAddress(
-        MODULE_SYSTEM.unwrap(),
-        s!("DirectInput8Create"),
-    )));
+    PROXY_FUNCTION = Some(transmute(GetProcAddress(module, s!("DirectInput8Create"))));
     println!("[INIT] DirectInput8Create");
 }
 
 unsafe fn init_console() {
     AllocConsole().unwrap_unchecked();
-    SetConsoleTitleA(s!("The Witcher 3: Console")).unwrap();
+    SetConsoleTitleW(w!("The Witcher 3: Console")).unwrap_unchecked();
 
     let stdout = GetStdHandle(STD_OUTPUT_HANDLE).unwrap();
     let stderr = GetStdHandle(STD_ERROR_HANDLE).unwrap();
@@ -99,20 +84,25 @@ unsafe fn init_console() {
     println!("The Witcher 3 - Axii {} - Plugin loader", VERSION);
 }
 
+fn init_tracing() {
+
+}
+
 #[no_mangle]
 #[allow(non_snake_case)]
-pub extern "system" fn DirectInput8Create(
+pub unsafe extern "system" fn DirectInput8Create(
     hinst: HINSTANCE,
-    dwVersion: DWORD,
-    riidltf: GUID,
-    ppvOut: LPVOID,
-    punkOuter: IUnknown,
-) {
+    dwversion: u32,
+    riidltf: *const GUID,
+    ppvout: *mut *mut c_void,
+    punkouter: IUnknown,
+) -> HRESULT {
     println!(
         "[CALL] DirectInput8Create ({:?}, {:?}, {:?}, {:?}, {:?})",
-        hinst, dwVersion, riidltf, ppvOut, punkOuter
+        hinst, dwversion, riidltf, ppvout, punkouter
     );
-
+    type DirectInput8CreateFunc =
+        extern "system" fn(HINSTANCE, u32, *const GUID, *mut *mut c_void, IUnknown) -> HRESULT;
     let func: DirectInput8CreateFunc = unsafe { transmute(PROXY_FUNCTION.unwrap()) };
-    func(hinst, dwVersion, riidltf, ppvOut, punkOuter);
+    func(hinst, dwversion, riidltf, ppvout, punkouter)
 }
