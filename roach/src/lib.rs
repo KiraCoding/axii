aa99330b93cb0675fbab96ea5014339c7f50df41
KiraCoding <38864051+KiraCoding@users.noreply.github.com>
2024-07-26 13:37:07 +0200

    Update lib.rs

--- roach/src/lib.rs
+++ roach/src/lib.rs
@@ -1,11 +1,12 @@
 use core::ffi::c_void;
 use std::mem::transmute_copy;
-use std::ptr::null;
 use std::sync::LazyLock;
 use std::thread::sleep;
 use std::time::Duration;
 use windows::core::w;
 use windows::{core::PCWSTR, Win32::System::LibraryLoader::GetModuleHandleW};
+
+struct NamesPoolTable {}
 
 struct CFunction {}
 struct NamesPool {}
@@ -29,7 +30,7 @@ pub unsafe extern "system" fn plugin() {
     let NamesPoolAddEntry: unsafe extern "C" fn(*mut NamesPool, PCWSTR) -> u32 =
         resolve_fn(0x145A3A0);
 
-    let CFunctionConstructor: unsafe extern "C" fn(
+    let CFunctionConstructor: unsafe extern "thiscall" fn(
         *mut CFunction,
         u32,
         *mut c_void,
@@ -37,7 +38,7 @@ pub unsafe extern "system" fn plugin() {
 
     let CRTTISystemGet: unsafe extern "C" fn() -> *mut CRTTISystem = resolve_fn(0x285D60);
 
-    let CRTTISystemRegisterGlobalFunction: unsafe extern "C" fn(*mut CRTTISystem, *mut CFunction) =
+    let CRTTISystemRegisterGlobalFunction: unsafe extern "thiscall" fn(*mut CRTTISystem, *mut CFunction) =
         resolve_fn(0x146A5f0);
 
     let names_pool = NamesPoolGet();
@@ -53,19 +54,20 @@ pub unsafe extern "system" fn plugin() {
 /// Calculates the offset from the base address of the calling process (.exe file).
 /// # Safety
 /// If any of the following conditions are violated, the result is Undefined Behavior:
-/// - The generic `F` must be an `unsafe extern "C" fn`.
-/// - The computed offset, in bytes, cannot overflow an isize.
+/// - The computed offset, in bytes, cannot overflow an `isize`.
 /// - The resulting function pointer from the computed offset must point to a function with the same signature as `F`.
 /// # Examples
 /// ```
-/// let offset = 0x2843A0;
-/// let add: unsafe extern "C" fn(u32, u32) -> u32 = resolve_fn(offset);
+/// type Add = unsafe extern "C" fn(u32, u32) -> u32;
 /// 
+/// let offset = 0x2843A0;
+/// let add: Add = resolve_fn(offset);
+///
 /// unsafe { assert_eq!(2, add(1, 1)); }
 /// ```
 #[must_use]
 #[inline(always)]
-pub unsafe fn resolve_fn<F>(offset: usize) -> F {
+pub unsafe fn resolve_fn<F: FnPtr>(offset: usize) -> F {
     struct Base(*mut c_void);
     unsafe impl Send for Base {}
     unsafe impl Sync for Base {}
@@ -73,8 +75,29 @@ pub unsafe fn resolve_fn<F>(offset: usize) -> F {
     // SAFETY: `GetModuleHandleW(null)` returns a handle to the current process, which is (presumably) always valid for the lifetime of the process.
     // https://learn.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-getmodulehandlew
     static BASE: LazyLock<Base> =
-        LazyLock::new(|| unsafe { Base(GetModuleHandleW(PCWSTR(null())).unwrap().0) });
-    
-    // SAFETY: The caller guarantees that `F` is an `unsafe extern "C" fn`.
+        LazyLock::new(|| unsafe { Base(GetModuleHandleW(PCWSTR::null()).unwrap_unchecked().0) });
+
+    // SAFETY: The caller guarantees that `F` is an `unsafe extern "ABI" fn`.
     unsafe { transmute_copy(&BASE.0.add(offset)) }
+}
+
+pub trait FnPtr {}
+
+macro_rules! impl_fnptr {
+    ($(($($args:ident),*)),*) => {
+        $(
+            impl<R, $($args),*> FnPtr for unsafe extern "C" fn($($args),*) -> R {}
+            impl<R, $($args),*> FnPtr for unsafe extern "cdecl" fn($($args),*) -> R {}
+            impl<R, $($args),*> FnPtr for unsafe extern "fastcall" fn($($args),*) -> R {}
+            impl<R, $($args),*> FnPtr for unsafe extern "thiscall" fn($($args),*) -> R {}
+        )*
+    };
+}
+
+impl_fnptr! {
+    (),
+    (A1),
+    (A1, A2),
+    (A1, A2, A3),
+    (A1, A2, A3, A4)
 }
