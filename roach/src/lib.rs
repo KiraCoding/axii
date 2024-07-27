2b5bf9ad7e70fc2c132efed1dfd148871870c327
KiraCoding <38864051+KiraCoding@users.noreply.github.com>
2024-07-27 13:11:43 +0200

    Update lib.rs

--- roach/src/lib.rs
+++ roach/src/lib.rs
@@ -1,15 +1,22 @@
 use core::ffi::c_void;
-use std::mem::transmute_copy;
-use std::sync::LazyLock;
 use std::thread::sleep;
 use std::time::Duration;
+use transcend::ptr::resolve_fn;
 use windows::core::w;
-use windows::{core::PCWSTR, Win32::System::LibraryLoader::GetModuleHandleW};
+use windows::core::PCWSTR;
 
-struct NamesPoolTable {}
+struct CNamesPoolTable {
+    get: unsafe extern "C" fn() -> *mut CNamesPool,
+    add_entry: unsafe extern "thiscall" fn(*mut CNamesPool, PCWSTR),
+}
+
+struct CRTTISystemTable {
+    get: unsafe extern "C" fn() -> *mut CRTTISystem,
+    register_global_function: unsafe extern "thiscall" fn(*mut CRTTISystem, *mut CFunction),
+}
 
 struct CFunction {}
-struct NamesPool {}
+struct CNamesPool {}
 struct CRTTISystem {}
 
 unsafe extern "C" fn my_callback(p1: u64, p2: u64, p3: u64) {
@@ -25,9 +32,9 @@ pub unsafe extern "system" fn plugin() {
     let Allocator: unsafe extern "C" fn(size: usize, alignment: usize) -> *mut CFunction =
         resolve_fn(0x2846B0);
 
-    let NamesPoolGet: unsafe extern "C" fn() -> *mut NamesPool = resolve_fn(0x2843A0);
+    let NamesPoolGet: unsafe extern "C" fn() -> *mut CNamesPool = resolve_fn(0x2843A0);
 
-    let NamesPoolAddEntry: unsafe extern "C" fn(*mut NamesPool, PCWSTR) -> u32 =
+    let NamesPoolAddEntry: unsafe extern "thiscall" fn(*mut CNamesPool, PCWSTR) -> u32 =
         resolve_fn(0x145A3A0);
 
     let CFunctionConstructor: unsafe extern "thiscall" fn(
@@ -38,8 +45,10 @@ pub unsafe extern "system" fn plugin() {
 
     let CRTTISystemGet: unsafe extern "C" fn() -> *mut CRTTISystem = resolve_fn(0x285D60);
 
-    let CRTTISystemRegisterGlobalFunction: unsafe extern "thiscall" fn(*mut CRTTISystem, *mut CFunction) =
-        resolve_fn(0x146A5f0);
+    let CRTTISystemRegisterGlobalFunction: unsafe extern "thiscall" fn(
+        *mut CRTTISystem,
+        *mut CFunction,
+    ) = resolve_fn(0x146A5f0);
 
     let names_pool = NamesPoolGet();
     let name_hash = NamesPoolAddEntry(names_pool, w!("TestFunction"));
@@ -49,55 +58,4 @@ pub unsafe extern "system" fn plugin() {
 
     let rtti_system = CRTTISystemGet();
     CRTTISystemRegisterGlobalFunction(rtti_system, function);
-}
-
-/// Calculates the offset from the base address of the calling process (.exe file).
-/// # Safety
-/// If any of the following conditions are violated, the result is Undefined Behavior:
-/// - The computed offset, in bytes, cannot overflow an `isize`.
-/// - The resulting function pointer from the computed offset must point to a function with the same signature as `F`.
-/// # Examples
-/// ```
-/// type Add = unsafe extern "C" fn(u32, u32) -> u32;
-/// 
-/// let offset = 0x2843A0;
-/// let add: Add = resolve_fn(offset);
-///
-/// unsafe { assert_eq!(2, add(1, 1)); }
-/// ```
-#[must_use]
-#[inline(always)]
-pub unsafe fn resolve_fn<F: FnPtr>(offset: usize) -> F {
-    struct Base(*mut c_void);
-    unsafe impl Send for Base {}
-    unsafe impl Sync for Base {}
-
-    // SAFETY: `GetModuleHandleW(null)` returns a handle to the current process, which is (presumably) always valid for the lifetime of the process.
-    // https://learn.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-getmodulehandlew
-    static BASE: LazyLock<Base> =
-        LazyLock::new(|| unsafe { Base(GetModuleHandleW(PCWSTR::null()).unwrap_unchecked().0) });
-
-    // SAFETY: The caller guarantees that `F` is an `unsafe extern "ABI" fn`.
-    unsafe { transmute_copy(&BASE.0.add(offset)) }
-}
-
-pub trait FnPtr {}
-
-macro_rules! impl_fnptr {
-    ($(($($args:ident),*)),*) => {
-        $(
-            impl<R, $($args),*> FnPtr for unsafe extern "C" fn($($args),*) -> R {}
-            impl<R, $($args),*> FnPtr for unsafe extern "cdecl" fn($($args),*) -> R {}
-            impl<R, $($args),*> FnPtr for unsafe extern "fastcall" fn($($args),*) -> R {}
-            impl<R, $($args),*> FnPtr for unsafe extern "thiscall" fn($($args),*) -> R {}
-        )*
-    };
-}
-
-impl_fnptr! {
-    (),
-    (A1),
-    (A1, A2),
-    (A1, A2, A3),
-    (A1, A2, A3, A4)
 }
