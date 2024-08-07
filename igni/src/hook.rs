856bbc2d559cc0d14c4e307ffc821f26f2a3959b
KiraCoding <38864051+KiraCoding@users.noreply.github.com>
2024-08-07 21:58:46 +0200

    wip

--- igni/src/hook.rs
+++ igni/src/hook.rs
@@ -1,107 +1,5 @@
 use core::ptr::{addr_of, copy_nonoverlapping};
-use std::ptr::null;
-use windows::Win32::System::Memory::{
-    VirtualAlloc, VirtualProtect, MEM_COMMIT, MEM_RELEASE, MEM_RESERVE, PAGE_EXECUTE_READWRITE,
-};
-
-pub trait Hookable<F> {
-    fn hook(&self, function: F);
-}
-
-macro_rules! impl_hookable {
-    ($(($($args:ident),*)),*) => {
-        $(
-            impl<F, R, $($args),*> Hookable<F> for unsafe extern "C" fn($($args),*) -> R
-            where
-                F: FnMut($($args),*)
-            {
-                fn hook(&self, function: F) {
-                    todo!()
-                }
-            }
-
-            impl<F, R, $($args),*> Hookable<F> for unsafe extern "cdecl" fn($($args),*) -> R
-            where
-                F: FnMut($($args),*)
-            {
-                fn hook(&self, function: F) {
-                    todo!()
-                }
-            }
-
-            impl<F, R, $($args),*> Hookable<F> for unsafe extern "win64" fn($($args),*) -> R
-            where
-                F: FnMut($($args),*)
-            {
-                fn hook(&self, function: F) {
-                    todo!()
-                }
-            }
-
-            impl<F, R, $($args),*> Hookable<F> for unsafe extern "fastcall" fn($($args),*) -> R
-            where
-                F: FnMut($($args),*)
-            {
-                fn hook(&self, function: F) {
-                    todo!()
-                }
-            }
-
-            impl<F, R, $($args),*> Hookable<F> for unsafe extern "thiscall" fn($($args),*) -> R
-            where
-                F: FnMut($($args),*)
-            {
-                fn hook(&self, function: F) {
-                    let ptr = addr_of!(self).cast();
-                    let function = addr_of!(function).cast();
-
-                    hook(ptr, function)
-                }
-            }
-        )*
-    };
-}
-
-fn alloc_page_near_addr(ptr: *const u8) -> *const u8 {
-    let addr = null();
-
-    let out_addr = unsafe {
-        VirtualAlloc(
-            Some(addr),
-            0,
-            MEM_COMMIT | MEM_RESERVE,
-            PAGE_EXECUTE_READWRITE,
-        )
-    };
-
-    out_addr.cast()
-}
-
-fn write_abs_jmp(mem: *const u8, function: *const usize) {
-    let mut abs_jmp = [
-        0x49, 0xBA, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x41, 0xFF, 0xE2,
-    ];
-    abs_jmp[2..10].copy_from_slice(&(function as u64).to_le_bytes());
-    unsafe { copy_rw(mem, abs_jmp.as_mut_ptr(), abs_jmp.len()) };
-}
-
-fn hook(ptr: *const u8, function: *const usize) {
-    let mut addr_to_jump = alloc_page_near_addr(ptr);
-    write_abs_jmp(addr_to_jump, function);
-
-    //32 bit relative jump opcode is E9, takes 1 32 bit operand for jump offset
-    let jmp_inst = [0xE9, 0x0, 0x0, 0x0, 0x0];
-    unsafe {
-        copy_nonoverlapping(
-            &mut addr_to_jump,
-            jmp_inst.as_mut_ptr().add(1),
-            size_of::<u32>(),
-        )
-    };
-
-    //install the hook
-    unsafe { copy_rw(jmp_inst.as_ptr(), ptr.cast_mut(), jmp_inst.len()) };
-}
+use windows::Win32::System::Memory::{VirtualProtect, PAGE_EXECUTE_READWRITE};
 
 pub unsafe fn copy_rw<T>(src: *const T, dst: *mut T, count: usize) {
     let size = count * size_of::<T>();
@@ -109,6 +7,49 @@ pub unsafe fn copy_rw<T>(src: *const T, dst: *mut T, count: usize) {
     unsafe { VirtualProtect(src.cast(), size, PAGE_EXECUTE_READWRITE, &mut old_protect).unwrap() };
     unsafe { copy_nonoverlapping(src, dst, count) };
     unsafe { VirtualProtect(src.cast(), size, old_protect, &mut old_protect).unwrap() };
+}
+
+pub trait Hookable<F> {
+    fn hook(&self, function: F) {
+        let ptr = addr_of!(self) as *mut u8;
+        let function = addr_of!(function) as usize;
+
+        let bytes = {
+            let mut jmp_bytes: [u8; 14] = [
+                0xFF, 0x25, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            ];
+            jmp_bytes[6..14].copy_from_slice(&function.to_le_bytes());
+            jmp_bytes
+        };
+
+        unsafe { copy_rw(bytes.as_ptr(), ptr, bytes.len()) };
+    }
+}
+
+macro_rules! impl_hookable {
+    ($(($($args:ident),*)),*) => {
+        $(
+            impl<F, R, $($args),*> Hookable<F> for unsafe extern "C" fn($($args),*) -> R
+            where
+                F: FnMut($($args),*) {}
+
+            impl<F, R, $($args),*> Hookable<F> for unsafe extern "cdecl" fn($($args),*) -> R
+            where
+                F: FnMut($($args),*) {}
+
+            impl<F, R, $($args),*> Hookable<F> for unsafe extern "win64" fn($($args),*) -> R
+            where
+                F: FnMut($($args),*) {}
+
+            impl<F, R, $($args),*> Hookable<F> for unsafe extern "fastcall" fn($($args),*) -> R
+            where
+                F: FnMut($($args),*) {}
+
+            impl<F, R, $($args),*> Hookable<F> for unsafe extern "thiscall" fn($($args),*) -> R
+            where
+                F: FnMut($($args),*) {}
+        )*
+    };
 }
 
 impl_hookable! {
